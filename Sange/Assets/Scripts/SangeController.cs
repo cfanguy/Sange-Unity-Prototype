e186c1f54c1f3f1f3d189e6779f00d400e701bcb
Chad M Fanguy <cfanguy@gmail.com>
2021-12-28 23:07:55 -0500

    Added player animations

--- Sange/Assets/Scripts/SangeController.cs
+++ Sange/Assets/Scripts/SangeController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class SangeController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(0, -1);
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
@@ -19,12 +25,25 @@ public class SangeController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) ||
+            !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
     }
 
     void FixedUpdate() {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x += speed * horizontal * Time.deltaTime;
+        position.y += speed * vertical * Time.deltaTime;
         
         rigidbody2d.MovePosition(position);
     }
